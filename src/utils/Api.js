9050cf423a07fe0cf6acbc4c14121526ff15b840
Genius Graphics & Infotech <geniusgraphicsdev07@gmail.com>
2025-04-09 05:32:49 +0530

    Update Api.js

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,5 +1,5 @@
 // export const Base_Url = "https://vivavista-backend.onrender.com/api";
 // export const Base_Url = "http://localhost:5001/api";
-// export const Base_Url = "https://vivavista-backend.onrender.com/api";
-export const Base_Url = "https://api.vivavistavacations.co.uk:5001/api";
+export const Base_Url = "https://vivavista-backend.onrender.com/api";
+// export const Base_Url = "https://api.vivavistavacations.co.uk:5001/api";
 // export const Base_Url = import.meta.env.VITE_API_BASE;
